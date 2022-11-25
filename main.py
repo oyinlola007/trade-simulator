86c1fa7baa73b4758d578894cd05d4adb22b61d4
Oyinlola <ojelere.ojelere@gmail.com>
2022-11-25 13:38:00 +0100

    .

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print(f"Logged in as {client.user}")
 
+
 @client.event
 async def on_message(message):
     if message.author.id != config.BOT_ID:
